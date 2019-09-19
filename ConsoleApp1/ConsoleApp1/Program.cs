586c788aa994414829841643cc21ef97db1f87c3
Mohamed <Mohamed@MSI>
2019-09-19 09:10:52 +0200

    TEST VIA VS 19

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("TEST GIT");
         }
     }
 }
