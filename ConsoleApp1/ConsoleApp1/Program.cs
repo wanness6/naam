384b66490425e31e8124dd5298b16c89b7224ff6
Mohamed <Mohamed@MSI>
2019-09-19 09:13:32 +0200

    VIA GITHWub

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("TEST GIT");
+            Console.WriteLine("LOGGED IN INTO GIT");
         }
     }
 }
